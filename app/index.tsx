89a3963ef845698a9cb54064005cdf462117cc64
AnthonyNet <zejda.antonin@gmail.com>
2023-10-18 23:47:13 +0200

    fix(add app folder to project, new install): brand new install with app folder

--- app/index.tsx
+++
@@ -1,3 +0,0 @@
-import Index from "./[...page]";
-
-export default Index;
--- components/Hero/Hero.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-
-
-export default function Hero() {
-	return (
-		<div>Hero</div>
-	);
-}
